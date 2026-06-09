64a4c8f25cc142552a2dae825b924406fd08e174
4L3SS104 <russoalessio2004@gmail.com>
2026-06-09 10:53:37 +0200

    ADD: modificato il ClayToggle per renderlo più fancy in dark mode

--- src/components/inputs/ClayToggle.vue
+++ src/components/inputs/ClayToggle.vue
@@ -46,9 +46,9 @@
 
 <template>
     <label ref="$el" class="clay-toggle">
-        <input type="checkbox" class="clay-toggle--input" />
-        <span class="clay-toggle--slider">
-            <span class="clay-toggle--knob"></span>
+        <input type="checkbox" class="clay-toggle__input" />
+        <span class="clay-toggle__slider">
+            <span class="clay-toggle__knob"></span>
         </span>
     </label>
 </template>
@@ -62,6 +62,7 @@
         --clay-toggle-color-off: oklch(from var(--clay-light-color) calc(l - 0.15) c h);
         --clay-toggle-color-on: var(--clay-primary-color);
         --clay-toggle-color-knob: var(--white);
+        --clay-toggle-color-knob-elevation: var(--black);
         --clay-toggle-color-knob-shadow: oklch(from var(--clay-toggle-color-on) calc(l - 0.25) c h);
         --clay-toggle-color-shadow: oklch(from var(--black) calc(l - 0.25) c h);
         --clay-toggle-color-outline: oklch(from var(--clay-primary-color) l c calc(h + 180));
@@ -84,7 +85,7 @@
         width: var(--clay-toggle-width);
         transition: transform var(--clay-ease-duration) var(--clay-ease-function);
 
-        .clay-toggle--input
+        .clay-toggle__input
         {
             height: 0;
             opacity: 0;
@@ -92,7 +93,7 @@
             width: 0;
         }
 
-        .clay-toggle--slider
+        .clay-toggle__slider
         {
             background-color: var(--clay-toggle-color-off);
             border-radius: var(--clay-toggle-height);
@@ -105,7 +106,7 @@
             transition: background-color var(--clay-ease-duration) var(--clay-ease-function),
                         box-shadow var(--clay-ease-duration) var(--clay-ease-function);
 
-            & > .clay-toggle--knob
+            & > .clay-toggle__knob
             {
                 background-color: var(--clay-toggle-color-knob);
                 --clay-toggle-color-knob-shadow: oklch(from var(--clay-toggle-color-knob) calc(l - 0.25) c h );
@@ -117,7 +118,8 @@
                 position: absolute;
                 transition: transform var(--clay-ease-duration) var(--clay-ease-function);
                 width: var(--clay-toggle-knob-size);
-                @include mixins.clay-shadow-elevation($intensity: 0.125);
+                @include mixins.clay-shadow-elevation($color:
+                var(--clay-toggle-color-knob-elevation), $intensity: 0.125);
                 transition: transform var(--clay-ease-duration) var(--clay-ease-function),
                         box-shadow var(--clay-ease-duration) var(--clay-ease-function);
 
@@ -136,11 +138,11 @@
             }
         }
 
-        .clay-toggle--input:checked + .clay-toggle--slider
+        .clay-toggle__input:checked + .clay-toggle__slider
         {
             background-color: var(--clay-toggle-color-on);
 
-            & > .clay-toggle--knob
+            & > .clay-toggle__knob
             {
                 transform: translateX(
                     calc(var(--clay-toggle-width) - var(--clay-toggle-knob-size) - 2 * var(--clay-toggle-knob-offset))
@@ -148,15 +150,15 @@
             }
         }
 
-        .clay-toggle--input:focus-visible + .clay-toggle--slider
+        .clay-toggle__input:focus-visible + .clay-toggle__slider
         {
             box-shadow: var(--clay-toggle-shadow),
                         functions.clay-outline($color: var(--clay-toggle-color-outline), $width: 0.2em);
         }
 
-        &:hover .clay-toggle--slider > .clay-toggle--knob
+        &:hover .clay-toggle__slider > .clay-toggle__knob
         {
-            @include mixins.clay-shadow-elevation($intensity: 0.25);
+            @include mixins.clay-shadow-elevation($color: var(--clay-toggle-color-knob-elevation), $intensity: 0.35);
         }
     }
 
@@ -174,6 +176,26 @@
         {
             --clay-toggle-color-off: oklch(from var(--clay-dark-color) calc(l + 0.15) c h);
             --clay-toggle-color-on: oklch(from var(--clay-primary-color) calc(l - 0.2) c h);
+            --clay-toggle-shadow: inset 0 0.25em 0.25em 0 rgba(from var(--clay-toggle-color-shadow) r g b / 0.125),
+                                  inset 0 -0.25em 0.25em 0 rgba(from var(--white) r g b / 0.075);
+            --clay-toggle-color-knob: oklch(from var(--white) calc(l - 0.125) c h);
+        }
+
+        .clay-toggle .clay-toggle__input:checked + .clay-toggle__slider > .clay-toggle__knob
+        {
+            --clay-toggle-color-knob: var(--white);
+            --clay-toggle-color-knob-elevation: var(--white);
+            box-shadow: 0 0 0.375em 0 rgba(from var(--clay-toggle-color-knob-elevation) r g b / 0.25);
+        }
+
+        .clay-toggle:hover .clay-toggle__input:checked + .clay-toggle__slider > .clay-toggle__knob
+        {
+            box-shadow: 0 0 0.375em 0 rgba(from var(--clay-toggle-color-knob-elevation) r g b / 0.5);
+        }
+
+        .clay-toggle .clay-toggle__input:checked + .clay-toggle__slider > .clay-toggle__knob::before
+        {
+            @include mixins.clay-shadow-puff($intensity: 0.75);
         }
     }
 </style>
